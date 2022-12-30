4a9f907533f56417599159e4cf68720cfd38b125
gaspromov <gaspromov@mail.ru>
2022-12-30 20:43:49 +0300

    [new] Preload strategy setted for AppModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NetworkAwarePreloadingServiceService } from './tools/services/network-aware-preloading-service.service';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: NetworkAwarePreloadingServiceService })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
