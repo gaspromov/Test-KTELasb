68c354933262fa87a9d1a681489b7b6774d332d9
gaspromov <gaspromov@mail.ru>
2022-12-30 20:44:51 +0300

    [change] Routes for AppModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NetworkAwarePreloadingServiceService } from './tools/services/network-aware-preloading-service.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./items-list/items-list.module').then(m => m.ItemsListModule) },
+  { path: 'item/:id', loadChildren: () => import('./item/item.module').then(m => m.ItemModule) },
+  { path: 'create', loadChildren: () => import('./item-form/item-form.module').then(m => m.ItemFormModule) },
+  { path: '**', redirectTo: '/' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { preloadingStrategy: NetworkAwarePreloadingServiceService })],
