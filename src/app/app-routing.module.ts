1c4eaac15a12cf99db625606d789b14d7c4a842b
gaspromov <gaspromov@mail.ru>
2023-01-06 01:26:06 +0300

    [route] Homepage link changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { NetworkAwarePreloadingServiceService } from './tools/services/network-aware-preloading-service.service';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./items-list/items-list.module').then(m => m.ItemsListModule) },
-  { path: 'item/:id', loadChildren: () => import('./item/item.module').then(m => m.ItemModule) },
+  { path: 'items', loadChildren: () => import('./items-list/items-list.module').then(m => m.ItemsListModule) },
+  { path: 'items/:id', loadChildren: () => import('./item/item.module').then(m => m.ItemModule) },
   { path: 'create', loadChildren: () => import('./item-form/item-form.module').then(m => m.ItemFormModule) },
-  { path: '**', redirectTo: '/' }
+  { path: '**', redirectTo: '/items' }
 ];
 
 @NgModule({
